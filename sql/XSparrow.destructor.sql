9b13ee2e1a915aa946149d37f25c6b23156bb758
Javi Carretero <fjcarretero@ximdex.com>
2013-07-11 12:48:49 +0200

    Update XSparrow.destructor.sql
    Restoring the 'Create Project' action when uninstalling XSparrow.

--- sql/XSparrow.destructor.sql
+++ sql/XSparrow.destructor.sql
@@ -1,1 +1,2 @@
-Delete from Actions where module = "XSparrow";
+DELETE FROM Actions where module = "XSparrow";
+INSERT INTO Actions VALUES (6011,5012,'Add new project','addfoldernode','create_proyect.png','Create a new node with project type',10,NULL,0,'');
